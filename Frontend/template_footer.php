ba466b6098f59f1d8f641128ab842a530b302f2a
MehdiLamatig <77162581+MehdiLamatig@users.noreply.github.com>
2021-04-05 19:56:33 +0200

    image

--- Frontend/template_footer.php
+++ Frontend/template_footer.php
@@ -1,5 +1,5 @@
-        <!-- <footer class="page-footer font-small" style="background-color: #00BFFF">
+        <footer class="page-footer font-small" style="background-color: #00BFFF">
             <div class="footer-copyright text-center py-3">© 2021 Copyright : KHOSROWSHAHI Sam LAMATIG Mehdi</div>
-        </footer> -->
+        </footer>
     </body>
 </html>
